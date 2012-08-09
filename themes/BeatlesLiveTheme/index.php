223fc3dc92293f6603266976be96c52ea992d55f
Bailey <bailey.r.smith@gmail.com>
2012-08-09 15:27:25 -0700

    Added movie bars to youtube video

--- themes/BeatlesLiveTheme/index.php
+++ themes/BeatlesLiveTheme/index.php
@@ -1,8 +1,10 @@
 <?php head(array('bodyid'=>'home')); ?>
 
 <div id="primary-index">
-
+	<div class="clear"></div>
+	<div class="moviebars"></div>
 	<div class= "youtube"><iframe width="700" height="525" src="http://www.youtube.com/embed/7pQPjpX82z8" frameborder="0" allowfullscreen></iframe></div>
+	<div class="moviebars"></div>
  
     
 </div><!-- end primary -->
