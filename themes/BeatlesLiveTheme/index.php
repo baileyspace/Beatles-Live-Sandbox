b75ee1d00585297e927606ffad41346897ac5d05
Bailey <bailey.r.smith@gmail.com>
2012-08-09 14:39:43 -0700

    Update video aspect ratio

--- themes/BeatlesLiveTheme/index.php
+++ themes/BeatlesLiveTheme/index.php
@@ -2,7 +2,7 @@
 
 <div id="primary-index">
 
-	<div class= "youtube"><iframe width="600" height="450" src="http://www.youtube.com/embed/7pQPjpX82z8" frameborder="0" allowfullscreen></iframe></div>
+	<div class= "youtube"><iframe width="900" height="675" src="http://www.youtube.com/embed/7pQPjpX82z8" frameborder="0" allowfullscreen></iframe></div>
  
     
 </div><!-- end primary -->
