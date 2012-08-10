f9450b4994676f7e5df57cfe0f0b9c95f7830212
Bailey <bailey.r.smith@gmail.com>
2012-08-09 18:23:07 -0700

    added infernal links

--- themes/BeatlesLiveTheme/index.php
+++ themes/BeatlesLiveTheme/index.php
@@ -5,8 +5,14 @@
 	<div class="moviebars"></div>
 	<div class= "youtube"><iframe width="600" height="450" src="http://www.youtube.com/embed/7pQPjpX82z8" frameborder="0" allowfullscreen></iframe></div>
 	<div class="moviebars"></div>
- 
+	<div class="beatlesimgs">
+		<a href= '<?php echo uri('contribution'); ?>' id='bimg1'>WORDSSS</a>
+		<a href= '<?php echo uri('collections'); ?>' id='bimg2'>WORDSSS</a>
+		<a href= '<?php echo uri('collections'); ?>' id='bimg3'>WORDSSS</a>
+		<!-- <?php echo link_to_collections(array('Go On Tour'));?> -->
+ 	</div>
     
 </div><!-- end primary -->
 
 <?php foot(); ?>
+<!-- ,array('id'=>'bllogo') -->
